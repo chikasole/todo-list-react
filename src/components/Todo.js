75982df333a4eb60d30422b9480b0304c4bd7cd9
chikasole <42386449+chikasole@users.noreply.github.com>
2022-05-30 07:45:47 +0300

    deleted unnecessary codes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,20 +2,13 @@ import React from "react";
 import { IconButton } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Modal from 'react-modal';
 
 const Todo = ({ text, status, todo, todos, setTodo }) => {
     const [modalIsOpen, setIsOpen] = React.useState(false);
  const deleteTodo = () => {
     setTodo(todos.filter((el) => el.id !== todo.id));
  };
- function openModal() {
-    setIsOpen(true);
-  }
 
-  function closeModal() {
-    setIsOpen(false);
-  }
  return (
     <div className="todo">
         <li className="todo-item">{text} - {status}</li>
